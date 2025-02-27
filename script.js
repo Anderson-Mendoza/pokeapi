5330932cf8a515b5798fcfd2ff8402f6b0aae95a
Anderson Mendoza <mendozaanderson1195@gmail.com>
2025-02-27 01:31:46 -0500

    API call changes

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
 const URL = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 const LISTA_POKE = document.getElementById("todos");
+const BTN_BUSCAR = document.getElementById("btn-buscar");
+const INPUT_BUSCAR = document.getElementById("input-buscar");
 
 
-async function fecthPokemon() {
+let pokemonList = [];
+
+
+async function fetchPokemon() {
     try {
-        fetch(URL)
-            .then(res => res.json())
-            .then(data => {
+        const response = await fetch(URL);
+        const data = await response.json();
 
-                const arr = data.results
-                console.log(arr);
+        const pokemonDetails = await Promise.all(data.results.map(async (poke) => {
+            const response = await fetch(poke.url)
+            return response.json()
+        })
+        );
 
-                arr.forEach(async (poke) => {
+        pokemonList = pokemonDetails
 
-                    const pokemonDetails = await fetch(poke.url).then(res => res.json());
+        mostrarPokemon(pokemonList)
 
-                    let container = document.createElement("div");
-                    container.classList.add("container-card-pokemon");
-
-                    let img = document.createElement("img");
-                    img.src = pokemonDetails.sprites.other["official-artwork"].front_default;
-                    img.alt = pokemonDetails.name
-                    img.classList.add("container-img");
-
-                    let p = document.createElement("p");
-                    p.textContent = pokemonDetails.name;
-                    p.classList.add("p");
-
-                    let numberId = document.createElement("p");
-                    numberId.textContent = `#${pokemonDetails.id.toString().padStart(3, 0)}`;
-
-                    container.appendChild(img);
-                    container.appendChild(p);
-                    container.appendChild(numberId);
-
-                    LISTA_POKE.appendChild(container);
-                });
-            })
     } catch (error) {
-        console.error("No se han cargado los recursos de la API: ", error);
+        console.error("No se han cargado los recursos de la API", error);
+
     }
 }
 
 
-fecthPokemon();
+function mostrarPokemon(pokemonArray) {
+
+    pokemonArray.forEach(pokemon => {
+        const container = document.createElement("div");
+        container.classList.add("container-card-pokemon");
+
+        const img = document.createElement("img");
+        img.src = pokemon.sprites.other["official-artwork"].front_default;
+        img.alt = pokemon.name;
+        img.classList.add("container-img");
+
+        const p = document.createElement("p");
+        p.textContent = pokemon.name;
+        p.classList.add("p");
+
+        const numberId = document.createElement("p");
+        numberId.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+        container.appendChild(img);
+        container.appendChild(p);
+        container.appendChild(numberId);
+
+        LISTA_POKE.appendChild(container);
+    });
+
+}
+
+fetchPokemon()
+
+
+
 
 
